8f2494b0158478c2af83110f39211a729a1710d4
nseakleang <sengseakleang@gmail.com>
2018-11-06 11:36:52 +0700

    dina

--- app/src/main/java/com/seakleang/homework/DinaActivity.java
+++ app/src/main/java/com/seakleang/homework/DinaActivity.java
@@ -3,11 +3,11 @@ package com.seakleang.homework;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class DianaActivity extends AppCompatActivity {
+public class DinaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_diana);
+        setContentView(R.layout.activity_dina);
     }
 }
